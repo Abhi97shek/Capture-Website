bca3e9b9fa2f6cde08db844fb2a69354ac2825c3
Abhi97shek <abhi.kumar736@gmail.com>
2022-01-21 18:49:45 +0530

    Adding the React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import AboutUs from './pages/AboutUs';
 import GlobalStyle from './components/GlobalStyle';
+import AboutUs from './pages/AboutUs';
 import NavBar from './components/NavBar';
+import OurWork from './pages/OurWork';
+import {Route,Routes} from "react-router-dom";
+import ContactUs from './pages/ContactUs';
 function App() {
   return (
       <>
         <GlobalStyle />
         <NavBar />
-        <AboutUs />
+        <Routes>
+            <Route path="/" element={<AboutUs />} exact />
+            <Route path="/work" element={<OurWork />} exact/>
+            <Route path="/contact" element={<ContactUs />} exact />
+        </Routes>
+        
+        
+        
       </>
   );
 }
---
+++ src/pages/OurWork.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import athlete from "../assets/img/athlete-small.png";
+import theracer from "../assets/img/theracer-small.png";
+import goodtimes from "../assets/img/goodtimes-small.png";
+const OurWork = () => {
+  return (
+
+        <Work>
+              <Movie>
+              <h2>The Athlete</h2>
+                <div className='line'></div>
+                <img src={athlete} alt='athlete' /> 
+              </Movie>
+              <Movie>
+              <h2>The Racer</h2>
+               <div className='line'></div>
+     
+                 <img src={theracer} alt='theracer' />
+        
+
+              </Movie>
+              <Movie>
+                    <h2>Good Times</h2>
+                      <div className='line'></div>
+                        <img src={goodtimes} alt='goodtimes' />
+              </Movie>
+
+        </Work>
+    );
+};
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2{
+    padding: 1rem 0rem;
+  }
+`;
+
+const Movie = styled.div`
+    padding-bottom: 10rem;
+    .line{
+      height: 0.5rem;
+      background-color: #cccccc;
+      margin-bottom: 3rem;
+    }
+    img{
+      width: 100%;
+      height: 70vh;
+      object-fit: cover ;
+    }
+`;
+export default OurWork;
